c4e38789943d7410d86ae79d591784d71bf1ca85
ququzone <xueping.yang@gmail.com>
2021-03-05 07:11:45 +0800

    change to async

--- api/account.ts
+++ api/account.ts
@@ -1,9 +1,10 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 import Antenna from 'iotex-antenna';
 
-export default function (req: NowRequest, res: NowResponse) {
+export default async function (req: NowRequest, res: NowResponse) {
   const antenna = new Antenna("http://api.testnet.iotex.one:80");
   const { address = 'io1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqd39ym7' } = req.query;
   // @ts-ignore
-  res.send(`account ${address} balance: ${(await antenna.iotx.getAccount({address})).accountMeta.balance}!`);
+  const account = await antenna.iotx.getAccount({address});
+  res.send(`account ${address} balance: ${account.accountMeta.balance}!`);
 };
