71ea13d9996ef31c7f6ea1f15adcc94e4fb883cd
ququzone <xueping.yang@gmail.com>
2021-03-04 21:13:21 +0800

    fix stringEth bug

--- api/address.ts
+++ api/address.ts
@@ -4,5 +4,5 @@ import { fromString } from 'iotex-antenna/lib/crypto/address'
 export default function (req: NowRequest, res: NowResponse) {
   const { address = 'io1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqd39ym7' } = req.query;
   // @ts-ignore
-  res.send(`eth address: ${fromString(address).stringEth}!`);
+  res.send(`eth address: ${fromString(address).stringEth()}!`);
 };
