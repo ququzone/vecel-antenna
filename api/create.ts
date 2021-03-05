8df66bb402fd3b03dea14d0c05594682ad7775fd
ququzone <xueping.yang@gmail.com>
2021-03-05 08:27:48 +0800

    change import antenna

--- api/create.ts
+++ api/create.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from '@vercel/node';
-import Antenna from 'iotex-antenna';
+import Antenna from 'iotex-antenna/lib';
 
 export default async function (req: NowRequest, res: NowResponse) {
   const antenna = new Antenna("http://api.testnet.iotex.one:80");
