72ec3633919aa64091a81e5bab3a80a163daa502
ququzone <xueping.yang@gmail.com>
2021-03-05 09:11:22 +0800

    add debug log

--- api/account.ts
+++ api/account.ts
@@ -2,9 +2,11 @@ import { NowRequest, NowResponse } from '@vercel/node';
 import RpcMethod from 'iotex-antenna/lib/rpc-method/node-rpc-method';
 
 export default async function (req: NowRequest, res: NowResponse) {
+  console.log("before new rpc method");
   const antenna = new RpcMethod("http://api.testnet.iotex.one:80");
   const { address = 'io1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqd39ym7' } = req.query;
 
+  console.log("before request rpc");
   // @ts-ignore
   const acct = antenna.getAccount({address});
 
