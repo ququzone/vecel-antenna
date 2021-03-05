63db56ccf2c22410fa6d9127984f6cff1ee4b07c
ququzone <xueping.yang@gmail.com>
2021-03-05 08:21:58 +0800

    add create account api

---
+++ api/create.ts
@@ -0,0 +1,10 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import Antenna from 'iotex-antenna';
+
+export default async function (req: NowRequest, res: NowResponse) {
+  const antenna = new Antenna("http://api.testnet.iotex.one:80");
+
+  const acct = antenna.iotx.accounts.create();
+
+  res.send(`created account: ${JSON.stringify(acct)}!`);
+};
