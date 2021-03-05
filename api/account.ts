079459f1336559233fdbc1cc03667f4a09f6b83b
ququzone <xueping.yang@gmail.com>
2021-03-05 09:22:08 +0800

    change to broswer rpc

--- api/account.ts
+++ api/account.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from '@vercel/node';
-import RpcMethod from 'iotex-antenna/lib/rpc-method/node-rpc-method';
+import RpcMethod from 'iotex-antenna/lib/rpc-method/browser-rpc-method';
 
 export default async function (req: NowRequest, res: NowResponse) {
   console.log("before new rpc method");
