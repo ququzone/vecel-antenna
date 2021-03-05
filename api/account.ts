ad7143369e90084cbf0f108f03701c18a939b588
ququzone <xueping.yang@gmail.com>
2021-03-05 09:26:13 +0800

    add browser env

--- api/account.ts
+++ api/account.ts
@@ -1,7 +1,10 @@
 import { NowRequest, NowResponse } from '@vercel/node';
+import browserEnv from "browser-env";
 import RpcMethod from 'iotex-antenna/lib/rpc-method/browser-rpc-method';
 
 export default async function (req: NowRequest, res: NowResponse) {
+  browserEnv();
+
   console.log("before new rpc method");
   const antenna = new RpcMethod("http://api.testnet.iotex.one:80");
   const { address = 'io1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqd39ym7' } = req.query;
