8a87b15aa773040c8494db1049fab6a46dafd77f
ququzone <xueping.yang@gmail.com>
2021-03-05 08:12:03 +0800

    change to async

--- api/account.ts
+++ api/account.ts
@@ -4,9 +4,11 @@ import Antenna from 'iotex-antenna';
 export default async function (req: NowRequest, res: NowResponse) {
   const antenna = new Antenna("http://api.testnet.iotex.one:80");
   const { address = 'io1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqd39ym7' } = req.query;
-  // @ts-ignore
-  const account = await antenna.iotx.getAccount({
-    address: "io183utcxzg0haqa7df2qqlltrwaf42wswj2rpu5w"
-  });
-  res.send(`account ${address} balance: ${JSON.stringify(account)}!`);
+
+  const acct = antenna.iotx.accounts.create();
+
+  // const account = await antenna.iotx.getAccount({
+  //   address: "io183utcxzg0haqa7df2qqlltrwaf42wswj2rpu5w"
+  // });
+  res.send(`account ${address} balance: ${JSON.stringify(acct)}!`);
 };
